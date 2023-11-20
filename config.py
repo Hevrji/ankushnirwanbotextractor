eb83367c1d7b1c5619e70cd9399f9122966934fc
Hevrji <116875997+Hevrji@users.noreply.github.com>
2023-11-20 21:26:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6955309259:AAFNPDLOBzYOmqgym9Slfj3pOD7LdZOD6UM"
+    BOT_TOKEN = "6776281397:AAEnzFbWI7JRwYKi3wHMm49685jUc1PeUNg"
     API_ID = "23321997"
     API_HASH = "3c0378a72f84d4dfe7d701bba5f3bbaa"
     AUTH_USERS = "5903548785"
